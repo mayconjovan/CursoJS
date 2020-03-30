f74cb37cbf9ecad390b0298002208c91685df973
Maycon J Pereira <mayconjovan@gmail.com>
2020-03-30 20:11:37 -0300

    :rocket: Desafio trocar imagem de fundo.

--- scripts/beluga.js
+++ scripts/beluga.js
@@ -3,10 +3,22 @@ function calcularMedia() {
     var qtd = arguments.length;
     var x = 0;
 
-    while(typeof arguments[x] === 'number'){
-    //while(typeof arguments[x]){ nao captura valores ZERO
-        total += arguments[x];                    
+    while (typeof arguments[x] === 'number') {
+        //while(typeof arguments[x]){ nao captura valores ZERO
+        total += arguments[x];
         x++;
     }
-    return total / qtd;
+    return (total / qtd).toFixed(2);
 }
+
+
+function sortear(n) {
+
+    var _n = n || 1;
+    var nSorteado = Math.random();
+    nSorteado = nSorteado * _n;
+    nSorteado = Math.floor(nSorteado);
+
+
+    return nSorteado;
+};
