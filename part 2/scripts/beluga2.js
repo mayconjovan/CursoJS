397a593d1011848fc908a36149e39c75d790064d
Maycon J Pereira <mayconjovan@gmail.com>
2020-04-07 18:56:06 -0300

    Metodos Proprios

--- part 2/scripts/beluga2.js
+++ part 2/scripts/beluga2.js
@@ -1,11 +1,32 @@
-
-
-var blg = (function(){
+var blg = (function () {
     var beluga = {};
     beluga.$ = document.querySelector.bind(document);
     beluga.$$ = document.querySelectorAll.bind(document);
 
+    Array.prototype.getMedia = function(){
+        return _calcularMedia(...this)
+    };
 
+
+    function _calcularMedia() {
+        let params = arguments;
+
+        let hasNaN = Array.prototype.some.call(params, (n) => isNaN(n));
+
+        if(hasNaN) throw new Error('somente números');
+
+        let total = Array.prototype.reduce.call(params, function(total, atual, i){
+            return total + parseFloat(atual);
+        })
+        return total / arguments.length;
+    }
+    beluga.calcularMedia = _calcularMedia;
+
+    function _each(seletor, fn){
+        var _sel = this.$$(seletor)
+        Array.prototype.forEach.call(_sel, fn)
+    }
+    beluga.each = _each;
     return beluga;
 
 })()
